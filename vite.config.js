80331b18f397f8a917527bc09250534d2360621d
saivivekmsv <saivivek.msv@gmail.com>
2024-06-12 20:07:38 +0530

    updated config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/new-portfolio/"
+  base: "/new-portfolio/",
+  // rollupOptions: {
+  //   input: {
+  //     main: resolve(__dirname, "index.html"),
+  //   },
+  // }
 })
